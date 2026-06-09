222aa4dd99b3196c46bfd946819cbc7d1b1856c4
calzado-kool <lhvlhv13@gmail.com>
2026-06-09 17:00:17 -0500

    Force cache bust v1781042415

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kool-pro-v1';
+const CACHE_NAME = 'kool-pro-1781042415';
 const APP_SHELL = ['/kool-pro/'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
