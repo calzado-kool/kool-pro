0410fe2af60bbaeac98da25629dffab483e08837
calzado-kool <lhvlhv13@gmail.com>
2026-06-09 22:19:36 -0500

    Restore stable backup v1781061574

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kool-pro-1781061417' + new Date().getTime();
+const CACHE_NAME = 'kool-pro-1781061574' + new Date().getTime();
 const ASSETS = [
   '/kool-pro/',
   '/kool-pro/index.html',
