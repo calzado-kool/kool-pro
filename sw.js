9396e53b35c89fe9222e203da3a55dac2459d257
calzado-kool <lhvlhv13@gmail.com>
2026-06-10 01:12:32 -0500

    Fix cache v1781071950

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kool-pro-1781071772' + new Date().getTime();
+const CACHE_NAME = 'kool-pro-1781071950' + new Date().getTime();
 const ASSETS = [
   '/kool-pro/',
   '/kool-pro/index.html',
