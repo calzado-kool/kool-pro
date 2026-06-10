e78222ce53c2c792f40c2cd3f5f54d6e70d6c69c
calzado-kool <lhvlhv13@gmail.com>
2026-06-09 22:30:07 -0500

    Banner UX: Button text changed to CORRE POR EL v1781062205

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kool-pro-1781062017' + new Date().getTime();
+const CACHE_NAME = 'kool-pro-1781062205' + new Date().getTime();
 const ASSETS = [
   '/kool-pro/',
   '/kool-pro/index.html',
